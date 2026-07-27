90aa4f501463111b3e81a555b63213754ea8822b
iki1uc <entertrainer@web.de>
2026-07-27 09:35:26 +0200

    Add VEC module for computing quadrat properties

---
+++ r.js
@@ -0,0 +1,15 @@
+export const VEC = {
+
+    compute(quadrat) {
+        return {
+            VEC: [
+                quadrat.A1,
+                quadrat.A2,
+                quadrat.A3,
+                quadrat.A4
+            ],
+            orbit: "360°",
+            axiom: 4
+        };
+    }
+};
