ed9313096444a64933ad1219a21a3638d3ae452e
iki1uc <entertrainer@web.de>
2026-07-27 09:34:20 +0200

    Add LAGE module with activation function

---
+++ b.js
@@ -0,0 +1,12 @@
+export const LAGE = {
+
+    activate(pool) {
+        return {
+            degree: 360,
+            percent: 100,
+            axiom: 4,
+            square: "LA-GE-HOME-ORT",
+            pool
+        };
+    }
+};
