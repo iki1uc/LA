564df241fc7a485d49dc5ef28cf8bae6efca6153
iki1uc <entertrainer@web.de>
2026-07-27 09:34:53 +0200

    Create s.js

---
+++ s.js
@@ -0,0 +1,13 @@
+export const LAGE_QUADRAT = {
+
+    live(pool) {
+        return {
+            A1: pool.HOME,
+            A2: pool.ORT,
+            A3: pool.LA,
+            A4: pool.GE,
+            form: "QUADRAT",
+            orbit: "360°"
+        };
+    }
+};
